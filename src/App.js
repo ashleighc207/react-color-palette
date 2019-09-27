f961a0b7603fbb1e4b52f23f91c207753ab1a08d
Ashleigh Christopher <ashleighc207@gmail.com>
2019-09-27 14:38:23 -0400

    install copy to clipboard feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Nav />
       <Palette {...seedColors[4]} />
     </div>
   );
--- src/Components/ColorBox.js
+++ src/Components/ColorBox.js
@@ -1,22 +1,25 @@
 import React, { Component } from "react";
 import "./ColorBox.css";
 import { Link } from "react-router-dom";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 class ColorBox extends Component {
   render() {
     const { name, color } = this.props;
     return (
-      <div className="ColorBox" style={{ backgroundColor: color }}>
-        <div className="ColorBox--info">
-          <span className="ColorBox--name">{name}</span>
-          <Link to={`/color/${color}`} className="ColorBox--link">
-            More...
-          </Link>
+      <CopyToClipboard text={color}>
+        <div className="ColorBox" style={{ backgroundColor: color }}>
+          <div className="ColorBox--info">
+            <span className="ColorBox--name">{name}</span>
+            <Link to={`/color/${color}`} className="ColorBox--link">
+              More...
+            </Link>
+          </div>
+          <div className="ColorBox--copy_container">
+            <button className="ColorBox--copy">Copy</button>
+          </div>
         </div>
-        <div className="ColorBox--copy_container">
-          <button className="ColorBox--copy">Copy</button>
-        </div>
-      </div>
+      </CopyToClipboard>
     );
   }
 }
