5f67cf2c16b46deb044c353ffcaa81fdf316e2af
Ashleigh Christopher <ashleighc207@gmail.com>
2019-10-14 16:58:23 -0400

    move styles out of colorbox component

--- src/Components/ColorBox.js
+++ src/Components/ColorBox.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import "./ColorBox.css";
 import { Link } from "react-router-dom";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import chroma from "chroma-js";
+import styles from "../Styles/ColorBoxStyles.js";
+import { withStyles } from "@material-ui/core/styles";
 
 class ColorBox extends Component {
   constructor(props) {
@@ -18,6 +19,7 @@ class ColorBox extends Component {
     });
   }
   render() {
+    const { classes } = this.props;
     let color;
     this.props.format === "hex"
       ? (color = this.props.hex)
@@ -26,14 +28,16 @@ class ColorBox extends Component {
       : (color = this.props.rgba);
     let visible, textColor;
     let luminance = chroma(color).luminance();
-    luminance < 0.15 ? (textColor = "light-text") : (textColor = "dark-text");
+    luminance < 0.15
+      ? (textColor = `${classes.lightText}`)
+      : (textColor = `${classes.darkText}`);
 
     this.state.copied ? (visible = "show") : (visible = "");
     let format = this.props.format;
     return (
-      <div className="ColorBox" style={{ backgroundColor: color }}>
-        <div className="ColorBox--info">
-          <span className={`ColorBox--name ${textColor}`}>
+      <div className={classes.ColorBox} style={{ backgroundColor: color }}>
+        <div className={classes.ColorBoxInfo}>
+          <span className={`${classes.ColorBoxName} ${textColor}`}>
             {this.props.name}
           </span>
           {!this.props.singlePalette && (
@@ -41,7 +45,7 @@ class ColorBox extends Component {
               to={`/palette/${this.props.paletteName.replace(/ /g, "-")}/${
                 this.props.id
               }/${this.props.format}`}
-              className={`ColorBox--link ${textColor}`}
+              className={`${classes.ColorBoxLink} ${textColor}`}
               onClick={e => e.stopPropagation()}
               format={format}
             >
@@ -50,21 +54,27 @@ class ColorBox extends Component {
           )}
         </div>
         <div
-          className={`ColorBox--overlay ${visible}`}
+          className={`${classes.ColorBoxOverlay} ${visible}`}
           style={{ backgroundColor: color }}
         ></div>
-        <div className={`ColorBox--overlay_text ${visible}`}>
-          <div className={`ColorBox--copied_banner ${textColor}`}>Copied!</div>
-          <div className={`ColorBox--copied_color ${textColor}`}>{color}</div>
+        <div className={`${classes.ColorBoxOverlayText} ${visible}`}>
+          <div className={`${classes.ColorBoxCopiedBanner} ${textColor}`}>
+            Copied!
+          </div>
+          <div className={`${classes.ColorBoxCopiedColor} ${textColor}`}>
+            {color}
+          </div>
         </div>
 
-        <div className="ColorBox--copy_container">
+        <div className={classes.ColorBoxCopyContainer}>
           <CopyToClipboard text={color} onCopy={this.showOverlay}>
-            <button className={`ColorBox--copy ${textColor}`}>Copy</button>
+            <button className={`${classes.ColorBoxCopy} ${textColor}`}>
+              Copy
+            </button>
           </CopyToClipboard>
         </div>
       </div>
     );
   }
 }
-export default ColorBox;
+export default withStyles(styles)(ColorBox);
