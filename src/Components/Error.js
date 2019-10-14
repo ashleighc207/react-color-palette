d6ef5fae9a063f92a3252aa5eca4b9528b61a354
Ashleigh Christopher <ashleighc207@gmail.com>
2019-10-14 16:28:47 -0400

    pull out all error styles

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,23 +1,25 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import "./Error.css";
+import styles from "../Styles/ErrorStyles.js";
+import { withStyles } from "@material-ui/core/styles";
 
 class Error extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <section className="Error">
-        <h1 className="Error--heading">
+      <section className={classes.Error}>
+        <h1 className={classes.ErrorHeading}>
           Oh no, looks like what you're looking for doesn't exist.
         </h1>
-        <h3 className="Error--subheading">Here's some other options:</h3>
-        <Link to="/" className="Error--link">
+        <h3 className={classes.ErrorSubheading}>Here's some other options:</h3>
+        <Link to="/" className={classes.ErrorLink}>
           View All Palettes
         </Link>
-        <Link to="/new-palette" className="Error--link">
+        <Link to="/new-palette" className={classes.ErrorLink}>
           Create a New Palette
         </Link>
       </section>
     );
   }
 }
-export default Error;
+export default withStyles(styles)(Error);
---
+++ src/Styles/ErrorStyles.js
@@ -0,0 +1,31 @@
+const styles = {
+  Error: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column"
+  },
+
+  ErrorHeading: {
+    fontFamily: '"Open Sans", sans-serif',
+    fontWeight: "600",
+    marginBottom: "1rem",
+    color: "#1b3191"
+  },
+
+  ErrorSubheading: {
+    fontFamily: '"Quicksand", sans-serif',
+    marginBottom: "1rem",
+    color: "#1b3191"
+  },
+
+  ErrorLink: {
+    fontFamily: '"Quicksand", sans-serif',
+    textDecoration: "none",
+    color: "#212121",
+    fontSize: "20px",
+    marginBottom: "0.5rem"
+  }
+};
+
+export default styles;
