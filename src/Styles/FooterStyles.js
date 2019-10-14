ba6deb7cc80c64ee62ed453712c0122da5f7fbb0
Ashleigh Christopher <ashleighc207@gmail.com>
2019-10-14 16:24:49 -0400

    pull out footer styles

---
+++ src/Styles/FooterStyles.js
@@ -0,0 +1,21 @@
+const styles = {
+  Footer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    fontFamily: '"Quicksand", sans-serif',
+    fontSize: "24px",
+    height: "8%",
+    color: "#212121"
+  },
+
+  FooterEmoji: {
+    marginRight: "20px"
+  },
+
+  FooterName: {
+    marginLeft: "20px"
+  }
+};
+
+export default styles;
